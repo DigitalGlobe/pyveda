39161e4d5bcf3ce56a1b2ce10e8d5511e3228a80
Jamie Polackwich <rjpolackwich@gmail.com>
2018-05-07 15:57:10 -0600

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyveda',
+      version='0.0.1',
+      description='Python API for interfacing with veda',
+      url='https://github.com/DigitalGlobe/pyveda',
+      license='MIT',
+      packages=['pyveda'],
+      zip_safe=False,
+      install_requires=[
+          "gbdxtools"
+        ]
+      )
