95597cec919f70c627c61cc4e9e94d8fc11eef5b
Jamie Polackwich <rjpolackwich@gmail.com>
2018-06-12 00:35:06 -0600

    utilities for managing temporary h5 files and filepaths

--- pyveda/utils.py
+++ pyveda/utils.py
@@ -2,6 +2,8 @@ import json
 import datetime
 import os
 import uuid
+import tempfile
+import shutil
 
 def mklogfilename(prefix, suffix="json", path=None):
     timestamp = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
@@ -18,6 +20,64 @@ def mktempfilename(prefix, suffix="h5", path=None):
     if path:
         filename = os.path.join(path, filename)
     return filename
+
+class NamedTemporaryHDF5File(object):
+    def __init__(self, prefix="veda", suffix="h5", path=None, delete=True):
+        self.name = mktempfilename(prefix, suffix=suffix, path=path)
+        self._delete = delete
+        self._fh = h5py.File(self.name, "a")
+
+    def __enter__(self):
+        return self._fh
+
+    def __exit__(self):
+        if self._delete:
+            self.remove()
+
+    def remove(self):
+        try:
+            self._fh.close()
+        except Exception:
+            pass
+        try:
+            os.remove(self.name)
+        except Exception:
+            pass
+
+class NamedTemporaryHDF5Generator(object):
+    def __init__(self, dirpath=None, delete=True, delete_files=True):
+        if not dirpath:
+            dirpath = tempfile.mkdtemp()
+        self.dirpath = dirpath
+        self._delete = delete
+        self._delete_files = delete_files
+        self._fps = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        if self._delete_files:
+            for h5 in self._fps:
+                h5.remove()
+        if self._delete:
+            self.remove()
+
+    def remove(self):
+        for h5 in self._fps:
+            h5.remove()
+        shutil.rmtree(self.dirpath)
+
+    def mktemp(self, prefix="veda", suffix="h5", delete=True):
+        h5 = NamedTemporaryHDF5File(prefix=prefix, suffix=suffix,
+                                                path=self.dirpath, delete=delete)
+        self._fps.append(h5)
+        return h5._fh, h5.name
+
+    def mktempfilename(self, prefix="veda", suffix="h5"):
+        return mktempfilename(prefix, suffix=suffix, path=self.dirpath)
+
+
 
 def extract_load_tasks(dsk):
     """
