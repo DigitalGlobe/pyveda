c70c53349cd1e0400359bb961613fe0498a4ed8b
Jamie Polackwich <rjpolackwich@gmail.com>
2018-07-12 13:27:28 -0600

    add option to schedule with native (ipython) asyncio loop

--- pyveda/fetch/compat/fetchpy3.py
+++ pyveda/fetch/compat/fetchpy3.py
@@ -1,7 +1,7 @@
 from pyveda.utils import extract_load_tasks
 from pyveda.fetch.aiohttp.client import ThreadedAsyncioRunner, AsyncBatchFetcher
 
-def write_fetch(points, labelgroup, datagroup):
+def write_fetch(points, labelgroup, datagroup, create_loop=True):
     reqs = []
     for p in points:
         url, token = extract_load_tasks(p.image.dask)
@@ -9,7 +9,10 @@ def write_fetch(points, labelgroup, datagroup):
         labelgroup.append(p.y)
 
     abf = AsyncBatchFetcher(reqs=reqs, token=token, on_result=datagroup.image.append)
-    with ThreadedAsyncioRunner(abf.run) as tar:
-        tar(loop=tar._loop)
+    if create_loop:
+        with ThreadedAsyncioRunner(abf.run) as tar:
+            tar(loop=tar._loop)
+    else:
+        abf.run()
 
 
