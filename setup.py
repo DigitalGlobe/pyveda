633dd036d396c64109d57b28b49b82aa1fccd47b
Jamie Polackwich <rjpolackwich@gmail.com>
2018-07-12 14:04:41 -0600

    conditional aiohttp install depending on py version

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import sys
 import os
 from setuptools import setup
 
+reqs = ["gbdxtools", "h5py", "tables"]
+if sys.version_info[0:2] >= (3,4):
+    reqs.append("aiohttp")
+
 setup(name='pyveda',
       version='0.0.1',
       description='Python API for interfacing with veda',
       url='https://github.com/DigitalGlobe/pyveda',
       license='MIT',
-      packages=['pyveda', "pyveda.fetch", "pyveda.fetch.compat", "pyveda.fetch.aiohttp"],
+      packages=['pyveda', "pyveda.fetch", "pyveda.fetch.compat", "pyveda.fetch.aiohttp", "pyveda.fetch.diagnostics"],
       zip_safe=False,
-      install_requires=[
-          "gbdxtools",
-          "h5py",
-          "tables"
-        ]
+      install_requires=reqs
       )
