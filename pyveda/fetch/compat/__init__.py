da1cec85c19e09d36bf5b6225932bf837f497449
Jamie Polackwich <rjpolackwich@gmail.com>
2018-07-12 13:26:47 -0600

    greater than not less than come on guy

--- pyveda/fetch/compat/__init__.py
+++ pyveda/fetch/compat/__init__.py
@@ -1,6 +1,6 @@
 import sys
 
-if sys.version_info[0:2] < (3,4):
+if sys.version_info[0:2] >= (3,4):
     from pyveda.fetch.compat.fetchpy3 import write_fetch
 else:
     from pyveda.fetch.compat.fetchpy2 import write_fetch
