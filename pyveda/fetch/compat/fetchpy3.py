dd79f52a56ad97eac6eefd41674476675500d2ad
Jamie Polackwich <rjpolackwich@gmail.com>
2018-07-12 13:51:58 -0600

    can't integrate with jupyter loop rn

--- pyveda/fetch/compat/fetchpy3.py
+++ pyveda/fetch/compat/fetchpy3.py
@@ -1,7 +1,7 @@
 from pyveda.utils import extract_load_tasks
 from pyveda.fetch.aiohttp.client import ThreadedAsyncioRunner, AsyncBatchFetcher
 
-def write_fetch(points, labelgroup, datagroup, create_loop=True):
+def write_fetch(points, labelgroup, datagroup):
     reqs = []
     for p in points:
         url, token = extract_load_tasks(p.image.dask)
@@ -9,10 +9,7 @@ def write_fetch(points, labelgroup, datagroup, create_loop=True):
         labelgroup.append(p.y)
 
     abf = AsyncBatchFetcher(reqs=reqs, token=token, on_result=datagroup.image.append)
-    if create_loop:
-        with ThreadedAsyncioRunner(abf.run) as tar:
-            tar(loop=tar._loop)
-    else:
-        abf.run()
+    with ThreadedAsyncioRunner(abf.run) as tar:
+        tar(loop=tar._loop)
 
 
