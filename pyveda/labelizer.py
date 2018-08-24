c09db70de4904da5e4a63794add147de1ded5e38
chloe.hampton <chloe.hampton@digitalglobe.com>
2018-08-24 16:14:37 -0600

    Improvements to object_vote. This method should work now.

--- pyveda/labelizer.py
+++ pyveda/labelizer.py
@@ -9,6 +9,17 @@ from gbdxtools import Interface, TmsImage, CatalogImage,IpeImage
 from rasterio import features
 from shapely.geometry.geo import shape
 
+def format_data(chips, polygons):
+    """formats chip/polygon data for object vote"""
+    formatted_chips = []
+    formatted_polygons = []
+    for i,a in enumerate(chips):
+        polys = polygons[i]
+        for b in polys:
+            formatted_chips.append(a)
+            formatted_polygons.append(b)
+    return(formatted_chips, formatted_polygons)
+
 class voting(object):
     """Methods for data generation and verification"""
 
@@ -38,9 +49,6 @@ class voting(object):
         Callback for the widget buttons.
         Appends an image chip and associated binary classification to a list,
         then re-generates the widget.
-
-        Note: Having problems here. I don't understand why the variables initialized with the class won't
-        pass into this method.
         """
         if b.description == 'Yes':
             self.samples.append((self.chips[self.index], [1]))
@@ -74,18 +82,7 @@ class voting(object):
             print('all objects have been labeled')
         return(self.samples)
 
-    def format_data(self):
-        """formats chip/polygon data for object vote"""
-        formatted_chips = []
-        formatted_polygons = []
-        for i,a in enumerate(self.chips):
-            polys = self.polygons[i]
-            for b in polys:
-                formatted_chips.append(a)
-                formatted_polygons.append(b)
-        return(formatted_chips, formatted_polygons)
-
-    def _handle_buttons_object(b, formatted_chips, formatted_polygons):
+    def _handle_buttons_object(self, b):
         """Not currently in love with this either, I'm not crazy about two different outputs.
            That being said, I am not sure what else to do--simply trash the negative samples?
            Use one list and add a binary component? Use one list and return negative samples with no polygons?
@@ -93,39 +90,41 @@ class voting(object):
 
         if b.description == 'Yes':
             print("positive sample")
-            self.positive_samples.append((formatted_chips[self.index], formatted_polygons[self.index]))
+            self.positive_samples.append((self.chips[self.index], self.polygons[self.index]))
             self.index += 1
         elif b.description == 'No':
             print("negative sample")
-            self.negative_samples.append((formatted_chips[self.index], formatted_polygons[self.index]))
+            self.negative_samples.append((self.chips[self.index], self.polygons[self.index]))
             self.index += 1
         elif b.description == 'Exit':
-            self.index = len(shps)
-        self.object_vote(formatted_chips, formatted_polygons)
+            self.index = len(self.polygons)
+            print("bye")
 
-    def _plot_polygons(formatted_polygons,formatted_chips):
+        self.object_vote()
+
+    def _plot_polygons(self):
         """polygon plotter for object_vote"""
-        ax=plt.subplot()
-        if np.size(formatted_polygons)==1:
-            pxb=formatted_chips.pxbounds(formatted_polygons)
-            ax.add_patch(patches.Rectangle((pxb[0],pxb[1]),(pxb[2]-pxb[0]),\
-                    (pxb[3]-pxb[1]),edgecolor='red',fill=False,lw=2))
+        ax = plt.subplot()
+        if np.size(self.polygons[self.index]) == 1:
+            pxb=self.chips[self.index].pxbounds(self.polygons[self.index])
+            ax.add_patch(patches.Rectangle((pxb[0], pxb[1]), (pxb[2] - pxb[0]),\
+                    (pxb[3] - pxb[1]), edgecolor = 'red', fill = False, lw=2))
 
-    def object_vote(self, formatted_chips, formatted_polygons):
+    def object_vote(self):
         """method for varifying segmentation or object detection data"""
         clear_output()
         buttons = self._create_buttons()
         for b in buttons:
-            b.on_click(voting._handle_buttons_object(b,formatted_chips, formatted_polygons))
-        if self.index < len(formatted_polygons):
-            print("%0.f  out of %0.f objects have been labeled" % (self.index, len(formatted_polygons)))
+            b.on_click(self._handle_buttons_object)
+        if self.index < len(self.polygons):
+            print("%0.f  out of %0.f objects have been labeled" % (self.index, len(self.polygons)))
             display(HBox(buttons))
             plt.figure(figsize=(7,7))
             ax=plt.subplot()
             ax.axis("off")
-            ax.imshow(formatted_chips[self.index].rgb(bands=[4,2,1]))
-            voting._plot_polygons(formatted_polygons[self.index], formatted_chips[self.index])
+            ax.imshow(self.chips[self.index].rgb(bands=[4, 2, 1]))
+            self._plot_polygons()
             plt.title('Is this a %s?' % self.class_type)
-        if indexShp>=len(shps):
+        if self.index >= len(self.polygons):
             print('all objects have been labeled')
-        return(self.positiveSamples, self.negativeSamples)
+        return(self.positive_samples, self.negative_samples)
