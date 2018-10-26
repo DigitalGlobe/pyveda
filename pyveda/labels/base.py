7f6d7f4fc0801c8ab2e6137ec00a48a5ea67b88b
Jamie Polackwich <rjpolackwich@gmail.com>
2018-10-26 16:05:17 -0600

    set payload handler functions to staticmethods to call from the class

--- pyveda/labels/base.py
+++ pyveda/labels/base.py
@@ -9,7 +9,8 @@ class BaseLabel(object):
     def __init__(self, imshape):
         self.imshape = imshape
 
-    def _get_transform(self, bounds, height, width):
+    @staticmethod
+    def _get_transform(bounds, height, width):
         return from_bounds(*bounds, width, height)
 
 
@@ -20,31 +21,38 @@ class ClassificationLabel(BaseLabel):
 class SegmentationLabel(BaseLabel):
     _default_dtype = np.float32
 
-    def _from_geo(self, item):
-        out_shape = self.imshape
-        xfm = self._get_transform(item['data']['bounds'], *out_shape)
+    @staticmethod
+    def from_geo(item, imshape):
+        out_shape = imshape
+        if len(imshape) == 3:
+            out_shape = imshape[-2:]
+        xfm = BaseLabel._get_transform(item['data']['bounds'], *out_shape)
         out_array = np.zeros(out_shape)
         value = 1
         for k, features in item['data']['label'].items():
-            out_array += self._create_mask(features, value, out_shape, xfm)
+            out_array += SegmentationLabel._create_mask(features, value, out_shape, xfm)
             value += 1
         return out_array
 
-    def _create_mask(self, shapes, value, _shape, tfm):
+    @staticmethod
+    def _create_mask(shapes, value, _shape, tfm):
         return rasterize(((shape(g), value) for g in shapes), out_shape=_shape, transform=tfm)
 
 
 class ObjDetectionLabel(BaseLabel):
     _default_dtype = np.float32
 
-    def _from_geo(self, item):
-        out_shape = self.imshape
-        xfm = self._get_transform(item['data']['bounds'], *out_shape)
+    @staticmethod
+    def from_geo(item, imshape):
+        out_shape = imshape
+        if len(imshape) == 3:
+            out_shape = imshape[-2:]
+        xfm = BaseLabel._get_transform(item['data']['bounds'], *out_shape)
         labels = []
         for k, features in item['data']['label'].items():
             class_labels = []
             for f in features:
-                b = shape(f).bounds
+                b = _shape(f).bounds
                 ll, ur = ~xfm * (b[0],b[1]), ~xfm * (b[2],b[3])
                 class_labels.append([*ll, *ur])
             labels.append(class_labels)
