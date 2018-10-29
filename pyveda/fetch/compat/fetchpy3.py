a62d82e5be55f0984ea5c7ef82b6c1cb92bc96d1
Jamie Polackwich <rjpolackwich@gmail.com>
2018-10-29 12:04:47 -0600

    FIX split data then check stacked index for number of datapoints

--- pyveda/fetch/compat/fetchpy3.py
+++ pyveda/fetch/compat/fetchpy3.py
@@ -1,21 +1,19 @@
 from functools import partial
 import os
-from tempfile import NamedTemporaryFile
 import numpy as np
-from skimage.io import imread
 from pyveda.fetch.aiohttp.client import ThreadedAsyncioRunner, VedaBaseFetcher
 
 def vedabase_batch_write(data, database=None, partition=[70, 20, 10]):
     trainp, testp, valp = partition
-    batch_size = data.shape[0]
+    images, labels = data
+    batch_size = labels.shape[0]
     ntrain = round(batch_size * (trainp * 0.01))
     ntest = round(batch_size * (testp * 0.01))
     nval = round(batch_size * (valp * 0.01))
-    images, labels = data
 
     # write training data
-    database.train.images.append(images[:ntrain]))
-    database.labels.append(labels[:ntrain]))
+    database.train.images.append(images[:ntrain])
+    database.train.labels.append(labels[:ntrain])
 
     # write testing data
     test_start = ntrain + 1
@@ -25,14 +23,14 @@ def vedabase_batch_write(data, database=None, partition=[70, 20, 10]):
 
     # write validation data
     val_start = test_stop + 1
-    database.validation.images.append(images[val_start:])
-    database.validation.labels.append(labels[val_start:])
+    database.validate.images.append(images[val_start:])
+    database.validate.labels.append(labels[val_start:])
 
 def build_vedabase(database, source, partition, total, token, label_threads=1, image_threads=10):
     abf = VedaBaseFetcher(source, total_count=total, token=token,
                           write_fn=partial(vedabase_batch_write, database=database, partition=partition),
-                          lbl_payload_handler=partial(database._label_klass.from_geo, imshape=database.image_shape),
                           img_payload_handler=database._image_klass.bytes_to_array,
+                          lbl_payload_handler=partial(database._label_klass.from_geo, imshape=database.image_shape),
                           num_lbl_payload_threads=label_threads, num_img_payload_threads=image_threads)
 
     with ThreadedAsyncioRunner(abf.run) as tar:
