8b4c04a55d53e61d9399a78804d59348a952bef3
Jamie Polackwich <rjpolackwich@gmail.com>
2018-10-29 16:39:08 -0600

    passing in batch_transforms, calling append_batch in the db write fn

--- pyveda/fetch/compat/fetchpy3.py
+++ pyveda/fetch/compat/fetchpy3.py
@@ -6,29 +6,31 @@ from pyveda.fetch.aiohttp.client import ThreadedAsyncioRunner, VedaBaseFetcher
 def vedabase_batch_write(data, database=None, partition=[70, 20, 10]):
     trainp, testp, valp = partition
     images, labels = data
-    batch_size = labels.shape[0]
+    batch_size = images.shape[0]
     ntrain = round(batch_size * (trainp * 0.01))
     ntest = round(batch_size * (testp * 0.01))
     nval = round(batch_size * (valp * 0.01))
 
     # write training data
-    database.train.images.append(images[:ntrain])
-    database.train.labels.append(labels[:ntrain])
+    database.train.images.append_batch(images[:ntrain])
+    database.train.labels.append_batch(labels[:ntrain])
 
     # write testing data
     test_start = ntrain + 1
     test_stop = ntrain + ntest
-    database.test.images.append(images[test_start:test_stop])
-    database.test.labels.append(labels[test_start:test_stop])
+    database.test.images.append_batch(images[test_start:test_stop])
+    database.test.labels.append_batch(labels[test_start:test_stop])
 
     # write validation data
     val_start = test_stop + 1
-    database.validate.images.append(images[val_start:])
-    database.validate.labels.append(labels[val_start:])
+    database.validate.images.append_batch(images[val_start:])
+    database.validate.labels.append_batch(labels[val_start:])
 
 def build_vedabase(database, source, partition, total, token, label_threads=1, image_threads=10):
     abf = VedaBaseFetcher(source, total_count=total, token=token,
                           write_fn=partial(vedabase_batch_write, database=database, partition=partition),
+                          img_batch_transform=database._image_klass._batch_transform,
+                          lbl_batch_transform=database._label_klass._batch_transform,
                           img_payload_handler=database._image_klass.bytes_to_array,
                           lbl_payload_handler=partial(database._label_klass.from_geo, imshape=database.image_shape),
                           num_lbl_payload_threads=label_threads, num_img_payload_threads=image_threads)
