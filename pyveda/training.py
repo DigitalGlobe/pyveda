c82ea7ed9b35e2a8f2b7d5489f91d4524eb32bdf
Jamie Polackwich <rjpolackwich@gmail.com>
2018-10-30 08:31:33 -0600

    changing to_dataset to store

--- pyveda/training.py
+++ pyveda/training.py
@@ -488,7 +488,7 @@ class VedaCollection(BaseSet):
         assert self.id is not None, 'You can only release a loaded VedaCollection. Call the load method first.'
         return self._release(version)
 
-    def to_dataset(self, size, fname, partition=[70, 20, 10], **kwargs):
+    def store(self, size, fname, partition=[70, 20, 10], **kwargs):
         """ Build an hdf5 database from this collection and return it as a DataSet instance """
         namepath, ext = os.path.splitext(fname)
         if ext != ".h5":
