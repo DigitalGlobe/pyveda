d786cf3a5bbabe594136f60e29c089d1f236e2dd
drwelby <marc.pfister@digitalglobe.com>
2018-11-06 13:49:36 -0700

    fix host

--- pyveda/tests/unit/test_datapoint.py
+++ pyveda/tests/unit/test_datapoint.py
@@ -4,6 +4,7 @@ Tests for DataPoint that don't rely on the server
 # path hack for veda installs
 import os, sys
 sys.path.append("..")
+os.environ["SANDMAN_API"] = "https://veda-api-development.geobigdata.io"
 
 from pyveda import DataPoint, VedaCollection
 import json
@@ -14,7 +15,6 @@ import unittest
 test_dir = os.path.dirname(__file__)
 test_json = os.path.join(test_dir, 'responses', 'datapoint.json')
 
-os.environ["SANDMAN_API"] = "https://veda-api-development.geobigdata.io"
 
 class DataPointTest(unittest.TestCase):
 
@@ -30,8 +30,8 @@ class DataPointTest(unittest.TestCase):
         assert dp.data['dataset_id'] == 'e91fb673-4a31-4221-a8ef-01706b6d9b63'
 
     def test_datapoint_fetch(self):
-        vc = VedaCollection('fake')
-        #vc = VedaCollection.from_id('e91fb673-4a31-4221-a8ef-01706b6d9b63')
+        #vc = VedaCollection('fake')
+        vc = VedaCollection.from_id('e91fb673-4a31-4221-a8ef-01706b6d9b63')
         dp = vc.fetch('ae91f7df-ae37-4d31-9506-d9176f50403c')
         self.assertTrue(isinstance(dp, DataPoint))
         assert dp.id == 'ae91f7df-ae37-4d31-9506-d9176f50403c' 
