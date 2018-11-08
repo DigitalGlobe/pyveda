c3e4f6acdfa1ccf1d78e16cc12c91b48924c4e66
martham93 <martha.morrissey@colorado.edu>
2018-11-08 11:32:35 -0700

    test updates, adding skip to vcr tests for VC

--- pyveda/tests/unit/test_datapoint.py
+++ pyveda/tests/unit/test_datapoint.py
@@ -25,28 +25,28 @@ class DataPointTest(unittest.TestCase):
     def test_datapoint(self):
         dp = DataPoint(self.json)
         self.assertTrue(isinstance(dp, DataPoint))
-        assertEqual(dp.id, 'ae91f7df-ae37-4d31-9506-d9176f50403c')
-        assertEqual(dp.mltype, 'classification')
-        assertEqual(dp.dtype, 'uint8') # based on init logic and sample json
-        assertEqual(dp.label, {"building": 0})
-        assertEqual(dp.bounds. [
+        self.assertEqual(dp.id, 'ae91f7df-ae37-4d31-9506-d9176f50403c')
+        self.assertEqual(dp.mltype, 'classification')
+        self.assertEqual(dp.dtype, 'uint8') # based on init logic and sample json
+        self.assertEqual(dp.label, {"building": 0})
+        self.assertEqual(dp.bounds, [
             -97.74107094008983,
             30.270496899310096,
             -97.74029824874955,
             30.271269590650377
         ])
-        assertEqual(dp.tile_coords, ["962", "179"])
-        assertEqual(type(shape(dp)), Polygon)
-        assertEqual(dp.data['dataset_id'] == 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
+        self.assertEqual(dp.tile_coords, ["962", "179"])
+        self.assertEqual(type(shape(dp)), Polygon)
+        self.assertEqual(dp.data['dataset_id'], 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
 
     def test_datapoint_fetch(self):
         #vc = VedaCollection('fake')
         vc = VedaCollection.from_id('e91fb673-4a31-4221-a8ef-01706b6d9b63')
         dp = vc.fetch('ae91f7df-ae37-4d31-9506-d9176f50403c')
-        assertisinstance(dp, DataPoint))
-        assertEqual(dp.id, 'ae91f7df-ae37-4d31-9506-d9176f50403c')
-        assertEqual(dp.mltype, vc.mltype)
+        self.assertTrue(isinstance(dp, DataPoint))
+        self.assertEqual(dp.id, 'ae91f7df-ae37-4d31-9506-d9176f50403c')
+        self.assertEqual(dp.mltype, vc.mltype)
         self.assertTrue(isinstance(shape(vc), Polygon))
-        assertEqual(dp.data['dataset_id'], 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
-        assertEqual(dp.meta['parent'], 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
-        assertEqual(dp.dtype, vc.dtype) # should inherit
+        self.assertEqual(dp.data['dataset_id'], 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
+        self.assertEqual(dp.meta['parent'], 'e91fb673-4a31-4221-a8ef-01706b6d9b63')
+        self.assertEqual(dp.dtype, vc.dtype) # should inherit
--- pyveda/tests/unit/test_vedacollection.py
+++ pyveda/tests/unit/test_vedacollection.py
@@ -11,6 +11,8 @@ import json
 from shapely.geometry import shape, box
 from shapely.geometry.polygon import Polygon
 import unittest
+import vcr
+from unittest import skip
 
 test_dir = os.path.dirname(__file__)
 test_json = os.path.join(test_dir, 'responses', 'vc.json')
@@ -46,16 +48,16 @@ class VedaCollectionTest(unittest.TestCase):
 
 
 # a valid vedacollection ID
-# VC_ID = 'LC80370302014268LGN00'
+VC_ID = 'LC80370302014268LGN00'
 # a valid datapoint from the above
-# DP_ID = 'LC80370302014268LGN00'
-#
-# def force(r1, r2):
-#     return True
-#
-# my_vcr = vcr.VCR()
-# my_vcr.register_matcher('force', force)
-# my_vcr.match_on = ['force']
+DP_ID = 'LC80370302014268LGN00'
+
+def force(r1, r2):
+    return True
+
+my_vcr = vcr.VCR()
+my_vcr.register_matcher('force', force)
+my_vcr.match_on = ['force']
 
 # How to use the mock_gbdx_session and vcr to create unit tests:
 # 1. Add a new test that is dependent upon actually hitting GBDX APIs.
@@ -65,56 +67,62 @@ class VedaCollectionTest(unittest.TestCase):
 # 5. Replace the real gbdx token with "dummytoken" again
 # 6. Edit the cassette to remove any possibly sensitive information (s3 creds for example)
 
+@skip("skipping for now")
+class VedaCollectionTest_vcr(unittest.TestCase):
+    @skip
+    def test_vedacollection_new(self):
+        vc = VedaCollection('name')
+        self.assertTrue(isinstance(vc, VedaCollection))
+        self.assertEqual(vc.imshape, (0, 256, 256))
+        self.assertEqual(vc.mlType, 'classification')
+        self.assertEqual(vc.bounds, None)
+        self.assertEqual(vc.count, 0)
+        pass
+    @skip
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vc_search.yaml', filter_headers=['authorization'])
+    def test_vc_search(self):
+        results = search()
+        vc = VedaCollection.from_doc(results[0])
+        self.assertTrue(isinstance(vc, VedaCollection))
+        self.assertGreater(vc.count, 0)
+        self.assertEqual(type(shape(vc)), 'Polygon')
+        pass
+    @skip
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vedacollection_id.yaml', filter_headers=['authorization'])
+    def test_vedacollection_id(self):
+        vc = VedaCollection.from_id(VC_ID)
+        self.assertEqual(vc.id, vc_id)
+        self.assertEqual(vc.imshape, (3, 128, 128))
+        self.assertEqual(vc.mlType,'classification')
+        pass
 
-# class VedaCollectionTest(unittest.TestCase):
-#
-#     def test_vedacollection_new(self):
-#         vc = VedaCollection('name')
-#         self.assertTrue(isinstance(vc, VedaCollection))
-#         assert vc.imshape == (0, 256, 256)
-#         assert vc.mlType == 'classification'
-#         assert vc.bounds == None
-#         assert vc.count == 0
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vc_search.yaml', filter_headers=['authorization'])
-#     def test_vc_search(self):
-#         results = search()
-#         vc = VedaCollection.from_doc(results[0])
-#         self.assertTrue(isinstance(vc, VedaCollection))
-#         assert vc.count > 0
-#         assert type(shape(vc)) == 'Polygon'
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vedacollection_id.yaml', filter_headers=['authorization'])
-#     def test_vedacollection_id(self):
-#         vc = VedaCollection.from_id(VC_ID)
-#         assert vc.id == vc_id
-#         assert vc.imshape == (3, 128, 128)
-#         assert vc.mlType == 'classification'
-#
-# class VCFetchTest(unittest.TestCase):
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch.yaml', filter_headers=['authorization'])
-#     def setUp(self):
-#         self.vc = VedaCollection.from_id(VC_ID)
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_index.yaml', filter_headers=['authorization'])
-#     def test_fetch_index(self):
-#         dp = self.vc.fetch_index(0)
+@skip("skipping for now")
+class VCFetchTest(unittest.TestCase):
 
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch.yaml', filter_headers=['authorization'])
+    def setUp(self):
+        self.vc = VedaCollection.from_id(VC_ID)
+        pass
 
-#         self.assertTrue(isinstance(dp, DataPoint))
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_points.yaml', filter_headers=['authorization'])
-#     def test_fetch_points(self):
-#         dps = vc.fetch_points(5)
-#         assert len(dps = 5)
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_points_index.yaml', filter_headers=['authorization'])
-#     def test_fetch_points_offset(self):
-#         dps = vc.fetch_points(10,5)
-#         assert len(dps = 10)
-#
-#     @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_ids.yaml', filter_headers=['authorization'])
-#     def test_fetch_ids(self):
-#         ids = vc.fetch_ids(page_size=50)
-#         assert len(ids) == 50
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_index.yaml', filter_headers=['authorization'])
+    def test_fetch_index(self):
+        dp = self.vc.fetch_index(0)
+        self.assertTrue(isinstance(dp, DataPoint))
+
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_points.yaml', filter_headers=['authorization'])
+    def test_fetch_points(self):
+        dps = vc.fetch_points(5)
+        self.assertEqual(len(dps), 5)
+        pass
+
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_points_index.yaml', filter_headers=['authorization'])
+    def test_fetch_points_offset(self):
+        dps = vc.fetch_points(10,5)
+        self.assertEqual(len(dps), 10)
+        pass
+
+    @my_vcr.use_cassette('tests/unit/cassettes/test_vcfetch_fetch_ids.yaml', filter_headers=['authorization'])
+    def test_fetch_ids(self):
+        ids = vc.fetch_ids(page_size=50)
+        self.assertEqual(len(ids), 50)
+        pass
