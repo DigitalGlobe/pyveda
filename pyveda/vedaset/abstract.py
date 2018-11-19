09c47b39215a9e1eee228af2590b4f316aab6912
Jamie Polackwich <rjpolackwich@gmail.com>
2018-11-19 03:28:51 -0500

    Introdcing vedaset module, housing data access structures. Migrated old db module there to new name store, added module support for streamer and defined an abstract vedaset abc for enforcing a consistent dataset-accessor interface for various io-based data producers, methods

---
+++ pyveda/vedaset/abstract.py
@@ -0,0 +1,88 @@
+from abc import ABC, abstractmethod
+
+class BaseVedaSequence(ABC):
+    """Low level data access api to homogeneous sequences of data in PyVeda"""
+
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __iter__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, idx):
+        raise NotImplementedError
+
+    def __setitem__(self, idx, val):
+        raise NotImplementedError
+
+
+class BaseVetaGroup(ABC):
+    """Pair-wise access patterns defined on a group of BaseVedaSequences"""
+
+    def __len__(self):
+        return len(self.images)
+
+    def __iter__(self, spec=None):
+        if not spec:
+            spec = slice(0, len(self)-1, 1)
+        gimg = self.images.__iter__(spec)
+        glbl = self.images.__iter__(spec)
+        while True:
+            yield (gimg.__next__(), glbl.__next__())
+
+    def __getitem__(self, idx):
+        if isinstance(spec, int):
+            return [self.images[spec], self.labels[spec]]
+        return list(self.__iter__(spec))
+
+    def __setitem__(self, idx, val):
+        raise NotImplementedError
+
+    @property
+    @abstractbasemethod
+    def images(self):
+        raise NotImplementedError
+
+    @property
+    @abstractbasemethod
+    def labels(self):
+        raise NotImplementedError
+
+
+class BaseVedaSet(ABC):
+    """Core representation of partitioned Machine-Learning datasets in PyVeda"""
+
+    @property
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def mltype(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def classes(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def train(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def test(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def validate(self):
+        raise NotImplementedError
+
+
