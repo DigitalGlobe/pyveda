499d5ca7126f112a06bbe2b1855a9e41a751c18d
Jamie Polackwich <rjpolackwich@gmail.com>
2018-11-26 13:11:20 -0500

    manual background fetch configuration, submit next request to fetcher before waiting on q when driving iterator interface, background source consumption configuration methods, define exhausted for groups and vedastream with callbacks, vedastream handler lut as class attr, spec vedaset length as buffer length for now

--- pyveda/vedaset/stream/vedastream.py
+++ pyveda/vedaset/stream/vedastream.py
@@ -2,8 +2,11 @@ import asyncio
 import collections
 import queue
 import threading
+import time
+from functools import partial
 
 from pyveda.fetch.aiohttp.client import VedaStreamFetcher
+from pyveda.fetch.handlers import NDImageHandler, ClassificationHandler, SegmentationHandler, ObjDetectionHandler
 from pyveda.vedaset.abstract import BaseVedaSet, BaseVedaGroup, BaseVedaSequence
 
 class VSGenWrapper(object):
@@ -11,6 +14,7 @@ class VSGenWrapper(object):
         self.vs = vs
         self.source = _iter
         self.stored = False
+        self._n = 0
 
     def __iter__(self):
         return self
@@ -29,7 +33,9 @@ class VSGenWrapper(object):
         if self.stored:
             self.stored = False
             return self.value
-        return next(self.source)
+        val =  next(self.source)
+        self._n += 1
+        return val
 
 
 class StreamingVedaSequence(BaseVedaSequence):
@@ -52,31 +58,36 @@ class StreamingVedaGroup(BaseVedaGroup):
         self._n_consumed = 0
         self._vset = vset
         self._exhausted = False
+        if allocated == 0:
+            self.exhausted = True
+
+    def __len__(self):
+        return len(self._vset._buf)
 
     def __iter__(self):
         return self
 
     def __getitem__(self, idx):
-        pass
-#        return self._vset._buf[idx]
+        return [self.images[idx], self.labels[idx]]
 
     def __next__(self):
         while self._n_consumed < self.allocated:
+           try:
+                nreqs = next(self._vst._gen)
+            except StopIteration:
+                pass
+            else:
+                asyncio.run_coroutine_threadsafe(self._vset._fetcher.produce_reqs(reqs=[nreqs]),
+                                                 loop=self._vset._loop)
+
             # The following get() blocks, as it should, when we're waiting for
             # the thread running the asyncio loop to fetch more data while the
             # source generator is not yet exhausted
-            dp = self._vset._q.get()
-            try:
-                nreq = next(self._vset._gen) # This ought to throw after the last get()
-            except StopIteration:
-                self._vset._on_exhausted()
-            else:
-                f = asyncio.run_coroutine_threadsafe(self._vset._fetcher.consume(reqs=[nreq]), loop=self._vset._loop)
-            finally:
-                self._n_consumed += 1
-                return dp
+            dps = self._vset._q.get()
+            self._n_consumed += 1
+            return dps
 
-        self._exhausted = True
+        self.exhausted = True
         raise StopIteration
 
     @property
@@ -92,69 +103,51 @@ class StreamingVedaGroup(BaseVedaGroup):
 
     @property
     def images(self):
-        images, _ = zip(*self._vset._buf)
-        return DataSequence(self, list(images))
+        imgs, _ = zip(*self._vset._buf)
+        return StreamingVedaSequence(imgs)
 
     @property
     def labels(self):
-        _, labels = zip(*self._vset._buf)
-        return DataSequence(self, list(labels))
+        _, lbls = zip(*self._vset._buf)
+        return StreamingVedaSequence(lbls)
 
 
 class VedaStream(BaseVedaSet):
-    def __init__(self, mltype, classes, count, gen, partition, bufsize, cachetype=collections.deque, loop=None):
+    _lbl_handler_map = {"classification": ClassificationHandler,
+                       "segmentation": SegmentationHandler,
+                       "object_detection": ObjDetectionHandler}
+
+    def __init__(self, mltype, classes, count, gen, partition, bufsize,
+                 image_shape, cachetype=collections.deque, auto_startup=False,
+                 auto_shutdown=False, fetcher=None, loop=None):
         self.partition = partition
         self.count = count
+        self.image_shape = image_shape
         self._mltype = mltype
         self._classes = classes
-        self._gen = VSGenWrapped(self, gen)
+        self._gen = VSGenWrapper(self, gen)
         self._bufsize = bufsize
+        self._auto_startup = auto_startup
+        self._auto_shutdown = auto_shutdown
         self._exhausted = False
         self._train = None
         self._test = None
         self._validate = None
 
-        self._q = queue.Queue()
-        if cachetype is collections.deque:
-            self._buf = cachetype(maxlen=bufsize)
-        if not loop:
-            loop = asyncio.new_event_loop()
+        self._fetcher = fetcher
         self._loop = loop
-        self._lock = asyncio.Lock(loop=loop)
-        self._fetcher = VedaStreamFetcher(self)
-        self._thread = threading.Thread(target=partial(self._fetcher.run_loop, loop=loop))
-        self._thread.start()
-        time.sleep(0.5) # Give the thread time to start, as well as the loop it's running inside
+        self._q = queue.Queue()
+        self._buf = cachetype(maxlen=bufsize)
+        self._thread = None
 
-        self._initialize_buffer()
+        self._img_handler_class = NDImageHandler
+        self._lbl_handler_class = self._lbl_handler_map[self._mltype]
 
-    def _initialize_buffer(self):
-        reqs = []
-        while len(reqs) < self._bufsize:
-            reqs.append(next(self._gen))
-        f = asyncio.run_coroutine_threadsafe(self._fetcher._initialize(reqs=reqs, loop=self._loop), loop=self._loop)
+        if auto_start:
+            self._start_consumer()
 
-    def _on_group_exhausted(self):
-        if not any(self.train.exhausted, self.test.exhausted, self.validate.exhausted):
-            self._exhausted = True
-
-    def _on_exhausted(self):
-        f = asyncio.run_coroutine_threadsafe(self._fetcher.kill_workers(), loop=self._loop)
-        f.result() # Wait for workers to shutdown gracefully
-        self._loop.call_soon_threadsafe(self._loop.stop)
-        self._thread.join()
-        self._loop.close()
-
-    @property
-    def exhausted(self):
-        return self._exhausted
-
-    @exhausted.setter
-    def exhausted(self, val):
-        assert isinstance(val, bool)
-        if val:
-            self._on_exhausted()
-        self._exhausted = val
+    def __len__(self):
+        return self._bufsize # Whatever for now
 
     @property
     def mltype(self):
@@ -181,5 +174,71 @@ class VedaStream(BaseVedaSet):
         if not self._validate:
             self._validate = StreamingVedaGroup(round(self.count*self.partition[2]*0.01), self)
         return self._validate
+
+    @property
+    def exhausted(self):
+        return self._exhausted
+
+    @exhausted.setter
+    def exhausted(self, val):
+        assert isinstance(val, bool)
+        if val:
+            self._on_exhausted()
+        self._exhausted = val
+
+    def _on_group_exhausted(self):
+        if all([self.train.exhausted, self.test.exhausted, self.validate.exhausted]):
+            self.exhausted = True
+
+    def _on_exhausted(self):
+        if self._auto_shutdown:
+            self._stop_consumer
+
+    def _initialize_buffer(self):
+        reqs = []
+        while len(reqs) < self._bufsize:
+            try:
+                reqs.append(next(self._gen))
+            except StopIteration:
+
+        f = asyncio.run_coroutine_threadsafe(self._fetcher.produce_reqs(reqs=reqs), loop=self._loop)
+        f.result()
+
+    def _configure_fetcher(self, **kwargs):
+        img_py_h = self._img_handler_class._payload_handler
+        lbl_py_h = partial(self._lbl_handler_class._payload_handler,
+                           klasses=self.classes, out_shape=self.image_shape)
+
+        self._fetcher = VedaStreamFetcher(self,
+                                          total_count=self.count,
+                                          img_payload_handler=img_py_h,
+                                          lbl_payload_handler=lbl_py_h,
+                                          **kwargs)
+
+    def _configure_worker(self, fetcher=None, loop=None):
+        if not self._fetcher:
+            self._fetcher = self._configure_fetcher()
+        if not loop:
+            loop = asyncio.new_event_loop()
+        self._loop = loop
+        self._thread = threading.Thread(target=partial(self._fetcher.run_loop, loop=loop))
+
+    def _start_consumer(self):
+        if not self._fetcher:
+            self._configure_fetcher()
+        if not self._thread:
+            self._configure_worker()
+
+        self._thread.start()
+        time.sleep(0.5)
+        self._consumer_fut = asyncio.run_coroutine_threadsafe(self._fetcher.start_fetch(self._loop),
+                                                              loop=self._loop)
+
+    def _stop_consumer(self):
+        f = asyncio.run_coroutine_threadsafe(self._fetcher.kill_workers(), loop=self._loop)
+        f.result() # Wait for workers to shutdown gracefully
+        self._loop.call_soon_threadsafe(self._loop.stop)
+        self._thread.join()
+        self._loop.close()
 
 
