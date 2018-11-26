32d3a5770369c6f3848ae19ffc6df8a7d11cf2db
Jamie Polackwich <rjpolackwich@gmail.com>
2018-11-26 13:01:48 -0500

    removing default group method defs, forcing the base group api with abstractmethods

--- pyveda/vedaset/abstract.py
+++ pyveda/vedaset/abstract.py
@@ -22,25 +22,6 @@ class BaseVedaSequence(ABC):
 class BaseVedaGroup(ABC):
     """Pair-wise access patterns defined on a group of BaseVedaSequences"""
 
-    def __len__(self):
-        return len(self.images)
-
-    def __iter__(self, spec=None):
-        if not spec:
-            spec = slice(0, len(self)-1, 1)
-        gimg = self.images.__iter__(spec)
-        glbl = self.images.__iter__(spec)
-        while True:
-            yield (gimg.__next__(), glbl.__next__())
-
-    def __getitem__(self, idx):
-        if isinstance(spec, int):
-            return [self.images[spec], self.labels[spec]]
-        return list(self.__iter__(spec))
-
-    def __setitem__(self, idx, val):
-        raise NotImplementedError
-
     @property
     @abstractmethod
     def images(self):
@@ -49,6 +30,21 @@ class BaseVedaGroup(ABC):
     @property
     @abstractmethod
     def labels(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __len__(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __iter__(self, spec):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, idx):
+        raise NotImplementedError
+
+    def __setitem__(self, idx, val):
         raise NotImplementedError
 
 
