147bad6ec3e1089665b40508efdb8b32081b2fa0
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-02 17:16:11 -0700

    adding search to main.py from vedacollection.py module

--- pyveda/main.py
+++ pyveda/main.py
@@ -1,6 +1,16 @@
 from pyveda.vedaset.util import _dataset_exists_in_veda
 from pyveda.exceptions import RemoteCollectionNotFound
 
+def search(params={}, host=HOST):
+    r = conn.post('{}/{}'.format(host, "search"), json=params)
+    r.raise_for_status()
+    try:
+        results = r.json()
+        return [VedaCollection.from_doc(s) for s in results]
+    except Exception as err:
+        print(err)
+        return []
+
 def load(dataset_id=None, dataset_name=None, filename=None):
     if not dataset_id or dataset_name or filename:
         raise ValueError("When calling pyveda.load, specify one of: dataset_id, dataset_name, or filename")
@@ -12,7 +22,7 @@ def load(dataset_id=None, dataset_name=None, filename=None):
                 return VedaSet.from_id(dataset_id)
     if filename:
         return VedaSet.from_store(filename)
-    raise RemoteCollectionNotFound("No Collection found on Veda for identifier: {}".format(identifier)
+    raise RemoteCollectionNotFound("No Collection found on Veda for identifier: {}".format(identifier))
 
 
 def load_existing():
@@ -23,3 +33,8 @@ def load_streamer():
 
 def load_store():
     pass
+
+def _dataset_exists_in_veda(*args, **kwargs):
+    pass
+
+
