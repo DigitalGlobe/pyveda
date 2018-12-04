f0283b4f676c3be6780b549fd84fb0a02700b560
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-04 13:40:40 -0700

    utils for verifying kywargs

--- pyveda/utils.py
+++ pyveda/utils.py
@@ -9,10 +9,33 @@ import tables
 import numpy as np
 import threading
 import warnings
-
+import inspect
 
 from shapely.geometry import box
 from affine import Affine
+
+
+def check_unexpected_kwargs(kwargs, **unexpected):
+    for key, message in unexpected.items():
+        if key in kwargs:
+            raise ValueError(message)
+
+def parse_kwargs(kwargs, *name_and_values, **unexpected):
+    values = [kwargs.pop(name, default_value)
+              for name, default_value in name_and_values]
+    if kwargs:
+        check_unexpected_kwargs(kwargs, **unexpected)
+        caller = inspect.stack()[1]
+        args = ', '.join(repr(arg) for arg in sorted(kwargs.keys()))
+        message = caller[3] + \
+            '() got unexpected keyword argument(s) {}'.format(args)
+        raise TypeError(message)
+    return tuple(values)
+
+def assert_kwargs_empty(kwargs):
+    # It only checks if kwargs is empty.
+    parse_kwargs(kwargs)
+
 
 def from_bounds(west, south, east, north, width, height):
     """Return an Affine transformation given bounds, width and height.
