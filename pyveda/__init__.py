2bf79c3c19ad63c53746025164430628fa506a28
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-04 13:41:33 -0700

    search and load main imports

--- pyveda/__init__.py
+++ pyveda/__init__.py
@@ -1,4 +1,3 @@
-from pyveda.training import VedaCollection, search
+from pyveda.main import search, load
 from pyveda.vedaset import VedaStream, VedaBase
 from pyveda.datapoint import DataPoint
-from pyveda.models import Model
