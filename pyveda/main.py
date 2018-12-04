2a674775fdaa2e71844b97fe6bcaad491f834090
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-04 13:48:04 -0700

    moving search

--- pyveda/main.py
+++ pyveda/main.py
@@ -1,37 +1,37 @@
-from pyveda.vedaset.util import _dataset_exists_in_veda
+#from pyveda.vedaset.util import _dataset_exists_in_veda
+import os
 from pyveda.exceptions import RemoteCollectionNotFound
+from pyveda.auth import Auth
+from pyveda.vedaset import VedaBase
+from pyveda.datapoint import DataPoint
+from pyveda.veda.api import search
+from pyveda.veda.api import VedaCollectionProxy
 
-def search(params={}, host=HOST):
-    r = conn.post('{}/{}'.format(host, "search"), json=params)
-    r.raise_for_status()
-    try:
-        results = r.json()
-        return [VedaCollection.from_doc(s) for s in results]
-    except Exception as err:
-        print(err)
-        return []
+gbdx = Auth()
+HOST = os.environ.get('SANDMAN_API', "https://veda-api.geobigdata.io")
+conn = gbdx.gbdx_connection
 
 def load(dataset_id=None, dataset_name=None, filename=None):
     if not dataset_id or dataset_name or filename:
         raise ValueError("When calling pyveda.load, specify one of: dataset_id, dataset_name, or filename")
     # Check for dataset on veda
-    identifier = dataset_id or dataset_name:
+    identifier = dataset_id or dataset_name
     if identifier:
-        dataset_id = _dataset_exists_in_veda(identifier):
-            if dataset_id:
-                return VedaSet.from_id(dataset_id)
+        dataset_id = _dataset_exists_in_veda(identifier)
+        if dataset_id:
+            return load_existing(dataset_id)
     if filename:
-        return VedaSet.from_store(filename)
+        return load_store(filename)
     raise RemoteCollectionNotFound("No Collection found on Veda for identifier: {}".format(identifier))
 
 
-def load_existing():
+def load_existing(*args, **kwargs):
     pass
 
-def load_streamer():
+def load_streamer(*args, **kwargs):
     pass
 
-def load_store():
+def load_store(*args, **kwargs):
     pass
 
 def _dataset_exists_in_veda(*args, **kwargs):
