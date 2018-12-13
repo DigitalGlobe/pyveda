cb071154b5f85f1d505cf76352b50031fa3cacc3
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-13 09:45:33 -0700

    updating naive load and adding store

--- pyveda/main.py
+++ pyveda/main.py
@@ -9,7 +9,7 @@ gbdx = Auth()
 HOST = os.environ.get('SANDMAN_API', "https://veda-api.geobigdata.io")
 conn = gbdx.gbdx_connection
 
-def load(dataset_id=None, dataset_name=None, filename=None):
+def load(dataset_id=None, dataset_name=None, filename=None, count=None, partition=[70,20,10], **kwargs):
     """
     Main interface to access to remote, local and synced datasets
     """
@@ -27,14 +27,24 @@ def load(dataset_id=None, dataset_name=None, filename=None):
     raise RemoteCollectionNotFound("No Collection found on Veda for identifier: {}".format(identifier))
 
 
-def load_existing(*args, **kwargs):
-    pass
+def store(dataset_id=None, dataset_name=None, count=None, partition=[70,20,10], **kwargs):
+    if not dataset_id or dataset_name:
+        raise ValueError("When calling pyveda.store, specify one of: dataset_id, dataset_name")
+    coll = load(dataset_id=dataset_id, dataset_name=dataset_name)
+    vb = VedaBase.from_coll(coll, count=count, partition=partition)
+    pgen = vb.ids()
+
+    build_vedabase(vb, gbdx.gbdx_connection.access_token, **kwargs)
+    vb.flush()
+    return vb
+
+def load_existing(vid, *args, **kwargs):
+    return VedaStream.from_id(vid, *args, **kwargs)
 
 def load_streamer(*args, **kwargs):
     pass
 
-def load_store(*args, **kwargs):
-    pass
-
+def load_store(filename):
+    return VedaBase.from_path(filename)
 
 
