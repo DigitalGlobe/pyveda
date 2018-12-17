7dac264281e5a6842e600f01c8cff7be69c8eeac
Jamie Polackwich <rjpolackwich@gmail.com>
2018-12-17 09:33:29 -0700

    vedaset class home, needs to be implemented

---
+++ pyveda/vedaset/vedaset.py
@@ -0,0 +1,10 @@
+from pyveda.vedaset.store.vedabase import H5DataBase
+from pyveda.vedaset.stream.vedastream import BufferedDataStream
+
+
+class VedaBase(H5DataBase):
+    pass
+
+
+class VedaStream(BufferedDataStream):
+    pass
