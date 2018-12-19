c0e75cf4a671a422d3c47a95ad78d0770b46d9e2
martham93 <martha.morrissey@colorado.edu>
2018-12-19 09:20:28 -0700

    test updates

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -18,8 +18,8 @@ class MainFunctionsTest(unittest.TestCase):
         id = 'a15f07fa-85d4-4af7-a22e-7e0c50c7dfb9'
         self.vcp = VedaCollectionProxy.from_id(id)
         self.h5 = './test.h5'
-
-
+        self.vs = vb.load(self.vcp)
+        
     def tearDown(self):
             try:
                 os.remove(self.h5)
@@ -40,18 +40,20 @@ class MainFunctionsTest(unittest.TestCase):
         #self.assertRaises(HTTPError, pv.dataset_exists, None, dataset_name = 'xyz')
 
     def test_load(self):
+        self.assertRaises(ValueError, pv.store, self.vcp)
+        self.assertRaises(HTTPError, pv.store, dataset_id = id, filename = self.h5)
         self.assertTrue(isinstance(pv.load('ce6c0c66-f679-4bb5-aa14-eaa1579fe282'), VedaStream))
 
     def test_store(self):
-        pass
+        self.assertRaises(ValueError, pv.store, self.vcp)
+        self.assertRaises(HTTPError, pv.store, dataset_id = id, filename = self.h5)
+        #self.assertTrue(isinstance(pv.store(dataset_id = 'a15f07fa-85d4-4af7-a22e-7e0c50c7dfb9', filename = self.h5, count = 10), VedaBase))
 
     def test_loadexisting(self):
         self.assertTrue(isinstance(pv.load(self.vcp), VedaStream))
 
     def test_loadstore(self):
-        self.assertRaises(ValueError, pv.store, self.vcp)
-        self.assertRaises(HTTPError, pv.store, dataset_id = id, filename = self.h5)
-        #self.assertTrue(isinstance(pv.store(dataset_id = 'a15f07fa-85d4-4af7-a22e-7e0c50c7dfb9', filename = self.h5, count = 10), VedaBase))
+        self.assertTrue(isinstance(pv.store(dataset_id = 'a15f07fa-85d4-4af7-a22e-7e0c50c7dfb9', filename = self.h5, count = 10), VedaBase))
 
     def test_createfromgeojson(self):
         pass
