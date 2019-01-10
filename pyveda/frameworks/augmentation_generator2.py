15db7af9c355f4a39d3af2e80dfa38e9e1bff044
martham93 <martha.morrissey@colorado.edu>
2019-01-10 16:02:26 -0700

    fixing formatting

--- pyveda/frameworks/augmentation_generator2.py
+++ pyveda/frameworks/augmentation_generator2.py
@@ -1,24 +1,25 @@
 import numpy as np
 
+
 class BaseGenerator():
-    def __init__(self, cache, shape = None, batch_size = 32, shuffle = True):
+
+    def __init__(self, cache, shape=None, batch_size=32, shuffle=True):
         self.cache = cache
         self.batch_size = batch_size
         self.index = 0
         self.shuffle = shuffle
         self.on_epoch_end()
 
-
     def build_batch(self, index):
         raise NotImplemented
 
     def __getitem__(self, index):
-        #yield self.build_batch(index)
+        # yield self.build_batch(index)
         return self.build_batch(index)
 
     def __next__(self):
         try:
-            item = self[self.index] #index???
+            item = self[self.index]  # index???
         except IndexError:
             raise StopIteration
         self.index += 1
@@ -29,7 +30,7 @@ class BaseGenerator():
 
     def on_epoch_end(self):
         '''update index for each epoch'''
-        #self.indexes = np.arange(len(self.list_ids)) self.list_ids not defined in baseclass
+        # self.indexes = np.arange(len(self.list_ids)) self.list_ids not defined in baseclass
         self.indexes = np.arange(len(self.cache))
         if self.shuffle:
             np.random.shuffle(self.indexes)
@@ -47,7 +48,7 @@ class BaseGenerator():
 
 class VedaStoreGenerator(BaseGenerator):
     def __init__(self, cache, batch_size):
-        super().__init__(cache, batch_size = batch_size, shuffle = True)
+        super().__init__(cache, batch_size=batch_size, shuffle=True)
         self.list_ids = [i for i in range(0, len(self.cache))]
         self.mltype = cache._trainer.mltype
         self.shape = cache._trainer.image_shape
@@ -58,20 +59,20 @@ class VedaStoreGenerator(BaseGenerator):
             raise IndexError("index is invalid")
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
         list_ids_temp = [self.list_ids[k] for k in indexes]
-        #print(list_ids_temp)
+        # print(list_ids_temp)
         X, y = self.data_generation(list_ids_temp)
-        #print("build vb batch")
+        # print("build vb batch")
         return X, y
 
     def data_generation(self, list_ids_temp):
         '''Generates data containing batch_size samples
         optionally pre-processes the data'''
 
-        X = np.empty((self.batch_size, *self.shape[::-1])) #issue with shape?
+        X = np.empty((self.batch_size, *self.shape[::-1]))  # issue with shape?
         if self.mltype == 'classification':
-            y = np.empty((self.batch_size), dtype=int) # needs classes
+            y = np.empty((self.batch_size), dtype=int)   # needs classes
         if self.mltype == 'segmentation':
-            y = np.empty((self.batch_size, *self.shape[1:])) # good
+            y = np.empty((self.batch_size, *self.shape[1:]))   # good
         if self.mltype == 'object_detection':
             y = []
 
@@ -82,21 +83,25 @@ class VedaStoreGenerator(BaseGenerator):
             if self.mltype == 'object_detection':
                 y.append(self.cache.labels[_id])
             if self.mltype == 'segmentation':
-                     # will need to adjust based on augmentation (flipping/rotation)
+                #  will need to adjust based on augmentation (flipping/rotation)
                 y[i, ] = self.cache.labels[_id]
         if self.mltype == 'object_detection':
             return X, np.array(y)
         else:
             return X, y
 
+
 class VedaStreamGenerator(BaseGenerator):
+    
     def __init__(self):
             # self.mltype = cache._vset.mltype
             # self.shape = cache._vset.image_shape
         raise NotImplementedError()
+
     def __len__(self):
         raise NotImplementedError()
     # '''Denotes the number of batches per epoch'''
     #     return int(np.floor(self.allocated)/self.batch_size)
+
     def build_batch(self):
         raise NotImplementedError()
