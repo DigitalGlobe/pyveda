78e7bedb7590ae1b9068547c19f7a3eccb2cef60
Martha Morrissey <marthamorrissey93@gmail.com>
2019-01-15 15:21:19 -0700

    match development branch

--- pyveda/legacy/datapoint.py
+++ pyveda/legacy/datapoint.py
@@ -66,12 +66,6 @@ class DataPoint(object):
         load = load_image(self.links["image"]["href"], token, self.imshape, dtype=self.dtype)
         return da.from_delayed(load, shape=self.imshape, dtype=self.dtype)
 
-    @property
-    def thumbnail(self):
-        """ Returns a delayed dask call for fetching a thumbnail for a data point """
-        token = gbdx.gbdx_connection.access_token
-        delay = load_image(self.links["thumbnail"]["href"], token, self.imshape, dtype=self.dtype)
-        return self._delayed_image(delay)
 
     def _delayed_image(self, delay):
         return da.from_delayed(delay, shape=self.imshape, dtype=self.dtype)
