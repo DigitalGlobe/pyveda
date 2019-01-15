36599b61e1aa244790b141c245c624ac76f102a5
Jamie Polackwich <rjpolackwich@gmail.com>
2019-01-14 17:23:47 -0700

    global config class for setting host as well as providing mixin class for global host access and instantiated connection

--- pyveda/config.py
+++ pyveda/config.py
@@ -1,10 +1,42 @@
 import os
-import sys
+from pyveda.auth import Auth
 
-_PROD = "https://veda-api.geobigdata.io"
-_DEV = "https://veda-api-development.geobigdata.io"
+PROD = "https://veda-api.geobigdata.io"
+DEV = "https://veda-api-development.geobigdata.io"
+LOCAL = "http://host.docker.internal:3002"
 
-HOST = os.environ.et('SANDMAN_API', "https://veda-api.geobigdata.io")
+class _Config:
+    def __init__(self, defhost=PROD):
+        self._HOST = os.environ.get("SANDMAN_API", defhost)
+
+    @property
+    def HOST(self):
+        return self._HOST
 
 
+config = _Config()
 
+def set_prod():
+    config._HOST = PROD
+
+def set_dev():
+    config._HOST = DEV
+
+def set_local():
+    config._HOST = LOCAL
+
+def set_host(host):
+    config._HOST = host
+
+
+class VedaConfig:
+    def __init__(self, conn=Auth().gbdx_connection):
+        self._conn = conn
+
+    @property
+    def host(self):
+        return config.HOST
+
+    @property
+    def conn(self):
+        return self._conn
