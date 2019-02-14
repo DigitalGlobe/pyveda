9eadff346261d88309784a8451ab1da2cac1f3e6
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-13 17:01:18 -0700

    make a vedaset utils mod for more specific

---
+++ pyveda/vedaset/utils.py
@@ -0,0 +1,7 @@
+from pyveda.utils import ignore_warnings
+import tables
+`
+ignore_NaturalNameWarning = partial(ignore_warnings,
+                                    _warning=tables.NaturalNameWarning)
+
+
