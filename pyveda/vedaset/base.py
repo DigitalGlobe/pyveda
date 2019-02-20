2a5fa8ba1ee452e2ee46fb7038c2cb14665e30ee
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-20 11:32:44 -0700

    proxying some attrs from basedataset to samplearray; moving the index management scheme directly to the basedataset class, where relevant props can be found (partition, count) and cb-registered so that array interfaces need only reference the vset proxy to get correct info like index params. Defining a _configure_instance() class that can be used for general setup, eg attempting to fix indices, etc in event everything needed is set; defining register_prophooks for prop registration - can take methodtype or funtiontype. Letting __init__ deal with various keyword args, and using a classmethod to actually set the props on the class with the provided values

--- pyveda/vedaset/base.py
+++ pyveda/vedaset/base.py
@@ -1,99 +1,21 @@
-from collections import namedtuple, defaultdict, OrderedDict
-from pyveda.vedaset.abstract import *
-from pyveda.fetch.handlers import get_label_handler, bytes_to_array
+from collections import defaultdict, OrderedDict
+from pyveda.vedaset.props import register_vprops, VBaseprops, DATAGROUPS
 from pyveda.exceptions import NotSupportedException
-from pyveda.vedaset.props import VSPropMap, is_iterator, DATAGROUPS
 import numpy as np
 
 
-def slices_from_partition(total, partition):
-    # partition sums to 100
-    allocations = [np.rint(total * p * 0.01) for p in partition]
-    nparts = len(partition)
-    idxs = []
-    start = 0
-    for i, alloc in enumerate(allocations):
-        stop = start + alloc if i + 1 < nparts else total
-        idxs.append((start, stop))
-        start = stop
-    return idxs
 
+class BaseSampleArray(object):
+    _proxied = ("mltype", "classes", "image_shape", "image_dtype",)
 
-class VirtualIndexManager(object):
-    IndexObj = namedtuple("IndexObj", "name start stop allocated")
-
-    def __init__(self, partition, count, groups):
-        self.partition = partition
-        self.count = count
-        self.groups = groups # This prescribes allocation ordering
-        self.set_indexes()
-
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            try:
-                return self.indexes[self.groups[key]]
-            except IndexError as e:
-                pass
-        if isinstance(key, str):
-            try:
-                return self.indexes[key]
-            except KeyError as e:
-                pass
-        else:
-            raise TypeError("Get keys are String or integer")
-        raise ValueError("Group name or index non existent") from e
-
-    def set_indexes(self):
-        # Set dict to proxy key val lookup access
-        self.indexes = dict()
-        idxs = slices_from_partition(self.count, self.partition)
-        for group, (start, stop) in zip(self.groups, idxs):
-            self.indexes[group] = self.IndexObj(name=group, start=start,
-                                           stop=stop, allocated=stop-start)
-        # Set instance vars for attribute access
-        for g, idx in self.indexes.items():
-            setattr(self, g, idx)
-
-    def update_spec(self, spec, val):
-        if spec not in ["partition", "count"]:
-            raise ValueError("arg[0] must be one of {partition, count}"))
-        setattr(self, spec, val)
-        self.set_indexes()
-
-
-class BaseVariableArray(ABCVariableIterator):
-    _vtyp = "BaseVariableArray"
-
-    def __init__(self, vset, arr):
+    def __init__(self, vset):
         self._vset = vset
-        # TODO Check arr iterable/iterator container-like w append(write)
-        self._arr = arr
-
-    @property
-    def _arr(self):
-        return self._arr_
-
-    @_arr.setter
-    def _arr(self, arr):
-        self._arr_ = iter(arr)
-
-
-class BaseSampleArray(ABCSampleIterator):
-    _vtyp = "BaseSampleArray"
-    _delegated = ["mltype", "classes", "image_shape", "image_dytpe"]
-
-    def __init__(self, vset, group):
-        self._vset = vset
-        self._dgroup = group
+        self._group = group
 
     def __getattr__(self, name):
-        if name in self._delegated:
+        if name in self._proxied:
             return self._vset.__getattribute__(name)
-        raise AttributeError("this no have {}".format(name))
-
-    @property
-    def allocated(self):
-        return self._vset._vim[self._dgroup].allocated
+        raise AttributeError
 
     @property
     def images(self):
@@ -103,57 +25,42 @@ class BaseSampleArray(ABCSampleIterator):
     def labels(self):
         return self._vset._lbl_arr
 
-    @images.setter
-    def images(self, val):
-        raise NotSupportedException("Array modification denied: Protecting dataset integrity")
-
-    @labels.setter
-    def labels(self, val):
-        raise NotSupportedException("Array modification denied: Protecting dataset integrity")
-
-    def batch_iter(self, batch_size):
-        while True:
-            batch = []
-            while len(batch) < batch_size:
-                batch.append(self.__next__())
-            yield batch
-
     def __getitem__(self, key):
-        return [self.images[key], self.labels[key]]
+        return [self.images[key],
+                self.labels[key]]
 
     def __len__(self):
-        return self.allocated
+        return self.images.allocated
 
     def __iter__(self):
-        raise NotImplementedError
+        return self
 
     def __next__(self):
-        raise NotImplementedError
+        return [self.images.__next__(),
+                self.labels.__next__()]
 
 
-@delegate_metadata_props()
-class BaseDataSet(ABCDataSet):
-    _vtyp = "BaseDataSet"
-    _groups = DATAGROUPS
-    _mdprops = VSPropMap
-    _fetch_class = NotImplemented
-    _sample_class = NotImplemented
-    _variable_class = NotImplemented
+@register_vprops()
+class BaseDataSet(object):
+    _vpropmap = dict([(kls.__vname__, kls) for
+                    kls in VBaseprops])
+    _vprops = VBaseprops
+    _sample_class = BaseSampleArray
+    _variable_class = BaseVariableArray
 
-    def __init__(self, **kwargs):
+    def __init__(self, groups=DATAGROUPS, **kwargs):
         # Define a call signature here using inspect TODO
-        self._vim_ = None
+        self.groups = groups
         self._train = None
         self._test = None
-        self._valiate = None
-        self._img_arr_ = None
-        self._lbl_arr_ = None
-        self._configure_instance()
+        self._validate = None
+        self._vidx_ = None
 
-        for k, v in kwargs.items():
-            if k not in self._mdprops:
-                raise ValueError("Unexpected initialization argument!")
-            self._set_dprop(self, k, v)
+        self._set_dprops(**kwargs)
+
+        self._prc = defaultdict(OpRegister)
+        self._configure_instance()
+        self._register_prophooks()
 
     @classmethod
     def _set_dprop(cls, obj, dname, dval):
@@ -161,35 +68,38 @@ class BaseDataSet(ABCDataSet):
             p = getattr(cls, dname)
             p.__set__(obj, dval)
         except Exception as e:
-            if val is not None:
-                raise
+            pass
+
+    def _set_dprops(self, quiet=False, **kwargs):
+        for k, v in kwargs.items():
+            if k not in self._vpropmap.keys():
+                if not quiet:
+                    raise ValueError(
+                        "Unexpected initialization argument!")
+            self._set_dprop(self, k, v)
 
     def _configure_instance(self):
-        # do things like register callbacks etc here
-        pass
+        if is_partitionable(self):
+            self._update_indexes()
+
+    def _register_prophooks(self):
+        self._prc.count.register(self._update_indexes)
+        self._prc.partition.register(self._update_indexes)
 
     @property
-    def _vim(self):
-        if self._vim_ is None:
-            try:
-                prt = self.partition
-                cnt = self.count
-                grps = self._groups
-            except AttributeError as ae:
-                raise AttributeError("Data attrs {count, partition, _groups} must be set to access VIM") from None
-            vim = VirtualIndexManager(prt, cnt, grps)
-            self._mdhooks.partition.register(vim.update_spec)
-            self._mdhooks.count.register(vim.update_spec)
-            self._vim_ = vim
-        return self._vim_
+    def _vidx(self):
+        if self._vidx_ is None:
+            self._update_indexes()
+        return self._vidx_
 
-    @property
-    def _img_handler_fn(self):
-        return bytes_to_array
-
-    @property
-    def _lbl_handler_fn(self):
-        return get_label_handler(self)
+    def _update_indexes(self):
+        if not is_partitionable(self):
+            self._vidx_ = None
+            raise NotImplementedError
+        self._vidx_ = OrderedDict()
+        indexes = slices_from_partition(self.count, self.partition)
+        for g, (start, stop) in zip(self.groups, indexes):
+            self._vidx_[g] = PartitionIndex(start, stop)
 
     @property
     def train(self):
@@ -232,5 +142,5 @@ class BaseDataSet(ABCDataSet):
         into a map that can be used for initializing other Vedatype-like things
         """
         return dict([(pname, getattr(self, pname)) for pname
-                     in self._mdprops.keys()])
+                     in self._vprops.keys()])
 
