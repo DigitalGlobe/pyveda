fa5d9634027a319f631e66798bfa67429f73f181
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-20 11:40:50 -0700

    configuring h5database to register attr write callbacks on all prop changes; hydrating props from disk when re-opening a file; simplyfying the h5variablearray class to derive from relevant interfaces

--- pyveda/vedaset/store/vedabase.py
+++ pyveda/vedaset/store/vedabase.py
@@ -2,93 +2,56 @@ import os
 from functools import partial
 import numpy as np
 import tables
-from pyveda.utils import ignore_warnings
+from pyveda.vedaset.utils import ignore_NaturalNameWarning as ignore_nnw
 from pyveda.exceptions import LabelNotSupported, FrameworkNotSupported
-from pyveda.vedaset.base import BaseVariableArray, BaseSampleArray, BaseDataSet
+from pyveda.vedaset.base import BaseDataSet, BaseSampleArray, SerializedVariableArray,
+                                PartitionedIndexArray, ArrayTransformPlugin
 from pyveda.frameworks.batch_generator import VedaStoreGenerator
 from pyveda.vedaset.store.arrays import get_array_handler, NDImageMixin
-from pyveda.fetch.aiohttp.client import VedaBaseFetcher
 #from pyveda.vv.labelizer import Labelizer
 
 
-ignore_NaturalNameWarning = partial(ignore_warnings,
-                                    _warning=tables.NaturalNameWarning)
-
-class HDF5PartitionedArray(BaseVariableArray):
+class H5StoreVariableArray(SerializedVariableArray,
+                           PartitionedIndexArray,
+                           ArrayTransformPlugin):
     """
     This wraps a pytables array with access determined
     by a contiguous index range given by two integers
     """
-    def __init__(self, vset, arr):
-        super(HDF5PartitionedArray, self).__init__(vset, arr)
-        self._start_ = None
-        self._stop_ = None
-        self._itr = None
+    def __init__(self, vset, group, arr, input_fn=None, output_fn=None):
+        super().__init__(vset, group, arr,
+                         input_fn=input_fn,
+                         output_fn=output_fn)
+
+        self._itr_ = None
 
     @property
-    def _start(self):
-        if self._start_ is None:
-            self._start_, self._stop_ = self._vset._update_vindex(self)
-        return self._start_
-
-    @property
-    def _stop(self):
-        if self._stop_ is None:
-            self._start_, self._stop_ = self._vset._update_vindex(self)
-        return self._stop_
-
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            spec = self._translate_idx(key)
-            return self._output_fn(self._arr.__getitem__(spec))
-        elif isinstance(key, slice):
-            spec = self._translate_slice(key)
-        return [self._output_fn(v) for v in self._arr.__getitem__(spec)]
+    def _itr(self):
+        if self._itr_ is None:
+            self._itr_ = self._arr.iterrows(self._start, self._stop)
+        return self._itr_
 
     def __iter__(self):
+        self._itr = self._arr.iterrows(self._start, self._stop)
         return self
 
     def __next__(self):
-        # The subtleties of the following line are important to understand:
-        # pytables Arrays return themselves in iter methods.
-        # The lib implementation of this effectively means that expected iter
-        # objs are the same obj, a singleton. That means usage of simulaneous
-        # multiple iterators on single array can result in unexpected behavior
-        # since there is always only one maintained instance of iter state. See
-        # issue https://github.com/PyTables/PyTables/issues/293
-        self._itr = self._arr.iterrows(self._start, self._stop)
-        return self._output_fn(self._itr.__next__())
-
-    def _translate_idx(self, vidx):
-        if vidx is None: # Bounce back None slice parts
-            return vidx
-        idx = vidx + self._start
-        if idx > self._stop:
-            raise IndexError("Index out of data range")
-        return idx
-
-    def _translate_slice(self, sli):
-        start, stop, step = sli.start, sli.stop, sli.step
-        start = self._translate_idx(start)
-        stop = self._translate_idx(stop)
-        # None means default to edges
-        if start is None:
-            start = self._start
-        if stop is None:
-            stop = self._stop
-        return slice(start, stop, step)
-
-    def append(self, item):
-        self._arr.append(self._input_fn(item))
-
-    def append_batch(self, items):
-        items = [self._input_fn(v) for v in items]
-        self._arr.append(items)
+       try:
+            return super().__next__()
+        except StopIteration as si:
+            self._itr_ = None
+            raise
 
 
-class WrappedSampleNode(BaseSampleArray):
+class H5StoreSampleArray(BaseSampleArray):
 
-    def batch_generator(self, batch_size, shuffle=True, channels_last=False, rescale=False, flip_horizontal=False, flip_vertical=False, **kwargs):
+    def batch_generator(self, batch_size,
+                        shuffle=True,
+                        channels_last=False,
+                        rescale=False,
+                        flip_horizontal=False,
+                        flip_vertical=False,
+                        **kwargs):
         """
         Generatates Batch of Images/Lables on a VedaBase partition.
         #Arguments
@@ -103,20 +66,11 @@ class WrappedSampleNode(BaseSampleArray):
                                 channels_last=channels_last, rescale=rescale,
                                 flip_horizontal=flip_horizontal, flip_vertical=flip_vertical, **kwargs)
 
-    def __getitem__(self, spec):
-        if isinstance(spec, int):
-            return [self.images[spec], self.labels[spec]]
-        else:
-            return list(self.__iter__(spec))
-
     def __iter__(self):
         # Reset internal state
         self.images.__iter__()
         self.labels.__iter__()
         return self
-
-    def __next__(self):
-        return [self.images.__next__(), self.labels.__next__()]
 
     def clean(self, count=None):
         """
@@ -129,14 +83,15 @@ class WrappedSampleNode(BaseSampleArray):
         Labelizer(self, mltype, count, classes).clean()
 
 
+
 class H5DataBase(BaseDataSet):
     """
     An interface for consuming and reading local data intended to be used with
     machine learning training
     """
     _fetch_class = VedaBaseFetcher
-    _sample_class = WrappedSampleNode
-    _variable_class = HDF5PartitionedArray
+    _sample_class = H5StoreSampleArray
+    _variable_class = H5StoreVariableArray
 
     def __init__(self, fname, title="SBWM", overwrite=False, mode="a", **kwargs):
 
@@ -148,17 +103,35 @@ class H5DataBase(BaseDataSet):
                 return
 
         super(H5DataBase, self).__init__(**kwargs)
+        self._fileh = tables.open_file(fname, mode=mode, title=title)
 
         self._build_filetree()
+
+    def _configure_instance(self):
+        super()._configure_instance()
+        H5IOAdapter.configure(self)
+
+    def _register_prophooks(self):
+        super()._register_prophooks()
+        wfn = lambda v, n: setattr(self._root._v_attrs, n, v)
+        self._prc.mltype.register(wfn)
+        self._prc.classes.register(wfn)
+        self._prc.image_shape.register(wfn)
+        self._prc.image_dtype.register(wfn)
+        self._prc.partition.register(wfn)
+        self._prc.count.register(wfn)
 
     def _load_existing(self, fname, mode="a"):
         if mode == "w":
             raise ValueError("Opening the file in write mode will overwrite the file")
         self._fileh = tables.open_file(fname, mode=mode)
+        p = dict([(name, self._attrs[name]) for name in self._attrs.f_list()])
+        self._set_dprops(quiet=True, **p)
+        self._register_prophooks()
 
     def _build_filetree(self):
         # Build group nodes
-        for name in self._groups:
+        for name in self.groups:
             self._fileh.create_group("/", name.lower(),
                                      "Records of ML experimentation phases")
         # Build table, array leaves
@@ -166,32 +139,17 @@ class H5DataBase(BaseDataSet):
         self._create_arrays()
 
     @property
+    def _attrs(self):
+        return self._fileh.root._v_attrs
+
+    @property
     def _root(self):
         return self._fileh.root
 
-    @ignore_NaturalNameWarning
+    @ignore_nnw
     def _create_arrays(self):
         self._img_arr.create_array(self, self._fileh.root, self.image_dtype)
         self._lbl_arr.create_array(self, self._fileh.root)
-
-    @ignore_NaturalNameWarning
-    def _create_tables(self, filters=tables.Filters(0)):
-        # Table col specs in the structure pytables wants
-        idx_cols = {"ids": tables.StringCol(36)}
-        feature_cols = dict([(klass, tables.UInt8Col(pos=idx + 1))
-                             for idx, klass in enumerate(self.classes)])
-
-        # Build main id index and feature tables on root
-        self._fileh.create_table(self._root, "sample_index", idx_cols,
-                                 "Constituent Datasample ID index", filters)
-        self._fileh.create_table(self._root, "feature_table", feature_cols,
-                                 "Datasample feature contexts", filters)
-
-        # Build tables on groups that can be used for recording id logs during
-        # model experimentation
-        for name, group in self._hgroups.items():
-            self._fileh.create_table(group, "sample_log", idx_cols,
-                                     "Datasample ID log", filters)
 
     @property
     def _img_arr(self):
@@ -205,10 +163,6 @@ class H5DataBase(BaseDataSet):
             lbl_handler_class = get_array_handler(self)
             self._lbl_arr_ = lbl_handler_class(self, self._fileh.root.labels)
         return self._lbl_arr_
-
-    @property
-    def _hgroups(self):
-        return {group._v_name: group for group in self._fileh.root._f_iter_nodes("Group")}
 
     def flush(self):
         self._fileh.flush()
