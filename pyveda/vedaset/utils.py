5456667a83c170c21ebcdfae947dd84985c7c5c8
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-21 08:25:30 -0700

    updating vedaset utils with some useful class function operations that come in handy with VedaSet context switching

--- pyveda/vedaset/utils.py
+++ pyveda/vedaset/utils.py
@@ -1,7 +1,11 @@
 from pyveda.utils import ignore_warnings
-import tables
 
 ignore_NaturalNameWarning = partial(ignore_warnings,
                                     _warning=tables.NaturalNameWarning)
 
+def extend_instance(obj, cls):
+    """Apply mixins to a class instance after creation"""
+    base_cls = obj.__class__
+    base_cls_name = obj.__class__.__name__
+    obj.__class__ = type(base_cls_name, (cls, base_cls),{})
 
