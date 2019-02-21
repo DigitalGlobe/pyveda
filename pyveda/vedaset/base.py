ab850ce5562e142dbacc7bd5696a5201c95a499d
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-21 08:20:34 -0700

    moving BaseVariableArray back where it belons; renaming our _<data>_arrays and _<data>_classes

--- pyveda/vedaset/base.py
+++ pyveda/vedaset/base.py
@@ -1,16 +1,89 @@
-from collections import defaultdict, OrderedDict
-from pyveda.vedaset.interface import BaseVariableArray, is_iterator,
-                                    slice_from_partition, is_partitionable
-from pyveda.vedaset.props import register_vprops, VBaseprops, DATAGROUPS
-from pyveda.exceptions import NotSupportedException
 import numpy as np
+from collections import defaultdict, OrderedDict
+from pyveda.exceptions import NotSupportedException
+from pyveda.vedaset.props import register_vprops, VBaseProps, DATAGROUPS
+from pyveda.vedaset.interface import is_iterator, slice_from_partition,
+                                        is_partitionable
+
+
+class WrappedIterator(object):
+    def __init__(self, arr):
+        self._source = arr
+
+    @property
+    def _source(self):
+        return self._source
+
+    @_source.setter
+    def _source(self, source):
+        if not is_iterator(source):
+            raise TypeError("Input source must be define iterator interface")
+        self._source = source
+
+    def __getitem__(self, obj):
+        return self._source.__getitem__(obj)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self._source)
+
+
+class BaseVariableArray(WrappedIterator):
+    def __init__(self, vset, group, arr):
+        self._vset = vset
+        self._group = group
+        self._source = arr
+
+    @property
+    def _vidx(self):
+        return getattr(self._vset._vidx, self._group)
+
+    @property
+    def _start(self):
+        return self._vidx.start
+
+    @property
+    def _stop(self):
+        return self._vidx.stop
+
+    @property
+    def allocated(self):
+        return self._vidx.allocated
+
+    def _gettr(self, obj):
+        return obj
+
+    def _settr(self, obj):
+        return obj
+
+    def append(self, obj):
+        obj = self._settr(obj)
+        self._source.append(obj)
+
+    def __getitem__(self, key):
+        obj = super(BaseVariableArray, self).__getitem__(key)
+        if isinstance(key, int):
+            return self._gettr(obj)
+        return type(obj)([self._gettr(d) for d in obj])
+
+    def __next__(self):
+        obj = super(BaseVariableArray, self).__next__()
+        return self._gettr(obj)
+
+    def __len__(self):
+        return self.allocated
 
 
 
 class BaseSampleArray(object):
-    _proxied = ("mltype", "classes", "image_shape", "image_dtype",)
+    _proxied = ("mltype",
+                "classes",
+                "image_shape",
+                "image_dtype",)
 
-    def __init__(self, vset):
+    def __init__(self, vset, group):
         self._vset = vset
         self._group = group
 
@@ -21,11 +94,11 @@ class BaseSampleArray(object):
 
     @property
     def images(self):
-        return self._vset._img_arr
+        return self._vset._image_array
 
     @property
     def labels(self):
-        return self._vset._lbl_arr
+        return self._vset._label_array
 
     def __getitem__(self, key):
         return [self.images[key],
@@ -42,16 +115,16 @@ class BaseSampleArray(object):
                 self.labels.__next__()]
 
 
+
 @register_vprops()
 class BaseDataSet(object):
     _vpropmap = dict([(kls.__vname__, kls) for
-                    kls in VBaseprops])
+                    kls in VBaseProps])
     _vprops = VBaseprops
     _sample_class = BaseSampleArray
     _variable_class = BaseVariableArray
 
     def __init__(self, groups=DATAGROUPS, **kwargs):
-        # Define a call signature here using inspect TODO
         self.groups = groups
         self._train = None
         self._test = None
@@ -59,7 +132,6 @@ class BaseDataSet(object):
         self._vidx_ = None
 
         self._set_dprops(**kwargs)
-
         self._prc = defaultdict(OpRegister)
         self._configure_instance()
         self._register_prophooks()
@@ -103,6 +175,20 @@ class BaseDataSet(object):
         for g, (start, stop) in zip(self.groups, indexes):
             self._vidx_[g] = PartitionIndex(start, stop)
 
+    def _unpack(self):
+        """
+        Instance method useful for unpacking an existing Vedatype-like thing
+        into a map that can be used for initializing other Vedatype-like things
+        """
+        return dict([(pname, getattr(self, pname)) for pname
+                     in self._vprops.keys()])
+
+    def __len__(self):
+        try:
+            return len(self._image_array)
+        except (TypeError, AttributeError):
+            return self.count
+
     @property
     def train(self):
         if self._train is None:
@@ -122,27 +208,11 @@ class BaseDataSet(object):
         return self._validate
 
     @property
-    def _img_arr(self):
+    def _image_array(self):
         raise NotImplementedError
 
     @property
-    def _lbl_arr(self):
+    def _label_array(self):
         raise NotImplementedError
 
-    def _configure_fetcher(self):
-        raise NotImplementedError
-
-    def __len__(self):
-        try:
-            return len(self._img_arr)
-        except (TypeError, AttributeError):
-            return self.count
-
-    def _unpack(self):
-        """
-        Instance method useful for unpacking an existing Vedatype-like thing
-        into a map that can be used for initializing other Vedatype-like things
-        """
-        return dict([(pname, getattr(self, pname)) for pname
-                     in self._vprops.keys()])
 
