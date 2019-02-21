e5539540986f8f9ab251c59cc1f35a3cd32e62a9
Jamie Polackwich <rjpolackwich@gmail.com>
2019-02-21 08:15:42 -0700

    well the last commit covered this work but it's about the way adapt_serializers works with h5store

--- pyveda/io/hdf5/serializers.py
+++ pyveda/io/hdf5/serializers.py
@@ -1,15 +1,35 @@
 import os
-import numpy as np
 import tables
+import numpy as np
 import ujson as json
-from pyveda.utils import mktempfilename, _atom_from_dtype
-from pyveda.exceptions import LabelNotSupported, FrameworkNotSupported
+from functools import partial
 from pyveda.vedaset.abstract import mtypes
+from pyveda.io.utils import _atom_from_dtype
+from pyveda.exceptions import LabelNotSupported, FrameworkNotSupported
 
 
-def H5StoreIOAdapter(vstore):
-    config = {}
+class IOH5Interface(object):
+    """
+    Base interface for defining the serialization
+    functions and methods that are used by H5Store
+    classes.
+    """
 
+    @staticmethod
+    def _input_fn(item):
+        return item
+
+    @staticmethod
+    def _output_fn(item):
+        return item
+
+    @staticmethod
+    def create_array(vset, group, arr_name=None):
+        raise NotImplementedError
+
+
+class IOImageArray(IOH5Interface):
+    mltype = np.ndarray
 
     @staticmethod
     def _input_fn(item):
@@ -21,15 +41,17 @@ def H5StoreIOAdapter(vstore):
         return item # what could this thing be, let it fail
 
     @staticmethod
-    def create_array(vset, group):
+    def create_array(vset):
         atom = _atom_from_dtype(vset.image_dtype)
         shape = list(vset.image_shape)
         shape.insert(0,0)
         shape = tuple(shape)
-        vset._fileh.create_earray(group, "images", atom=atom, shape=shape)
+        vset._fileh.create_earray(vset._root,
+                                  "images", atom=atom, shape=shape)
 
 
-class ClassificationMixin(IOLabelMixin):
+class IOBinaryClassification(IOH5Interface):
+    mltype = "classification"
 
     @staticmethod
     def _input_fn(item):
@@ -39,24 +61,27 @@ class ClassificationMixin(IOLabelMixin):
         return item.reshape(1, *dims)
 
     @staticmethod
-    def create_array(vset, group):
+    def create_array(vset):
         atom = tables.UInt8Atom()
         shape = (0, len(vset.classes))
-        vset._fileh.create_earray(group, "labels", atom=atom, shape=shape)
+        vset._fileh.create_earray(vset._root,
+                                  "labels", atom=atom, shape=shape)
 
 
-class SegmentationMixin(IOLabelMixin, NDImageMixin):
+class IOInstanceSegmentation(IOH5Interface):
+    mltype = "segmentation"
 
     @staticmethod
-    def create_array(vset, group, dtype=np.uint8):
-        atom = _atom_from_dtype(dtype)
+    def create_array(vset):
+        atom = _atom_from_dtype(np.int8)
         shape = tuple([s if idx > 0 else 0 for
                        idx, s in enumerate(vset.image_shape)])
-        vset._fileh.create_earray(group, "labels", atom=atom, shape=shape)
+        vset._fileh.create_earray(vset._root,
+                                  "labels", atom=atom, shape=shape)
 
 
-class ObjDetectionMixin(IOLabelMixin):
-    _default_dtype = np.float32
+class IOObjectDetection(IOH5Interface):
+    mltype = "obj_detection"
 
     @staticmethod
     def _input_fn(item):
@@ -73,16 +98,49 @@ class ObjDetectionMixin(IOLabelMixin):
             vset.append(item)
 
     @staticmethod
-    def create_array(vset, group, filters=tables.Filters(complevel=0)):
+    def create_array(vset, filters=tables.Filters(complevel=0)):
         atom = tables.UInt8Atom()
-        vset._fileh.create_vlarray(group, "labels", atom=atom, filters=filters)
+        vset._fileh.create_vlarray(vset._root,
+                                   "labels", atom=atom, filters=filters)
 
 
-class
-def get_array_handler(inst):
-    if inst.mltype == "classification":
-        return ClassificationMixin
-    if inst.mltype == "segmentation":
-        return SegmentationMixin
-    return ObjDetectionMixin
+def adapt_serializers(vset):
+    """
+    Takes a HDF5-based DataStore object and sets
+    DataStore._variable_class with the approprite
+    serializer io functions based on the datatype
+    of the store.
+    """
+
+    image_ios = (IOImageArray,)
+    label_ios = (IOBinaryClassification,
+                 IOInstanceSegmentation,
+                 IOObjectDetection,)
+
+    def get_serializer(ios):
+        try:
+            return [io for io in ios if
+                    mltypes.types_match(vset, io)].pop()
+        except IndexError:
+            raise MLTypeError(
+                "'{}' is not of type 'mltypes.mltype'".format(vset)) from None
+
+    def adapt_serializer(io):
+        adapted = partial(vset._variable_class,
+                          input_fn = io._input_fn,
+                          output_fn = io._output_fn)
+        setattr(adapted, "create_array",
+                partial(io.create_array, vset))
+        return adapted
+
+    img_io = list(image_ios).pop()
+    img_class = adapt_serializer(img_io)
+    setattr(vset, "_image_class", img_class)
+
+    lbl_io = get_serializer(label_ios)
+    lbl_class = adapt_serializer(lbl_io)
+    setattr(vset, "_label_class", lbl_class)
+
+    return vset
+
 
